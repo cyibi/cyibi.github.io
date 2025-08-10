5bd10a854734231157e5962f4827d6f013ed61b2
cyibi <cyibila@gmail.com>
2025-08-10 22:21:21 +0900

    Create genreDefinition.js

---
+++ genreDefinition.js
@@ -0,0 +1,44 @@
+const genreDefinitions = {
+  game: {
+    label: "ゲーム",
+    key: "game",
+    showAd: false,
+    timeLimit: 0
+  },
+  anime: {
+    label: "アニメ",
+    key: "anime",
+    showAd: false,
+    timeLimit: 0
+  },
+  animal: {
+    label: "動物",
+    key: "animal",
+    showAd: true,
+    timeLimit: 30
+  },
+  character: {
+    label: "キャラクター",
+    key: "character",
+    showAd: true,
+    timeLimit: 30
+  },
+  youtube: {
+    label: "YouTube",
+    key: "youtube",
+    showAd: true,
+    timeLimit: 30
+  },
+  intermediate_game: {
+    label: "中級ゲーム",
+    key: "game",
+    showAd: true,
+    timeLimit: 20
+  },
+  advanced_game: {
+    label: "上級ゲーム",
+    key: "game",
+    showAd: true,
+    timeLimit: 15
+  }
+};
