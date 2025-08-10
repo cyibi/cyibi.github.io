fb0cda5b525dea664941e7ece87e6c84a3881aa3
cyibi <cyibila@gmail.com>
2025-08-10 22:27:20 +0900

    Update genreDefinition.js

--- genreDefinition.js
+++ genreDefinition.js
@@ -3,42 +3,49 @@ const genreDefinitions = {
     label: "ゲーム",
     key: "game",
     showAd: false,
-    timeLimit: 0
+    timeLimit: 0,
+    showVideo: false
   },
   anime: {
     label: "アニメ",
     key: "anime",
     showAd: false,
-    timeLimit: 0
+    timeLimit: 0,
+    showVideo: false
   },
   animal: {
     label: "動物",
     key: "animal",
     showAd: true,
-    timeLimit: 30
+    timeLimit: 30,
+    showVideo: true
   },
   character: {
     label: "キャラクター",
     key: "character",
     showAd: true,
-    timeLimit: 30
+    timeLimit: 30,
+    showVideo: true
   },
   youtube: {
     label: "YouTube",
     key: "youtube",
     showAd: true,
-    timeLimit: 30
+    timeLimit: 30,
+    showVideo: true
   },
   intermediate_game: {
     label: "中級ゲーム",
     key: "game",
     showAd: true,
-    timeLimit: 20
+    timeLimit: 20,
+    showVideo: true
   },
   advanced_game: {
     label: "上級ゲーム",
     key: "game",
     showAd: true,
-    timeLimit: 15
+    timeLimit: 15,
+    showVideo: true
   }
 };
