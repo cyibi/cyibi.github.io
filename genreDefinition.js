381639a40cad9775c361da2f0c2ae355728f02c2
cyibi <cyibila@gmail.com>
2025-08-10 22:38:53 +0900

    Update genreDefinition.js

--- genreDefinition.js
+++ genreDefinition.js
@@ -19,7 +19,7 @@ const genreDefinitions = {
     showAd: true,
     timeLimit: 30,
     showVideo: true,
-    videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+    videoUrl: "https://www.youtube.com/embed/0Bmhjf0rKe8?autoplay=1"
   },
   character: {
     label: "キャラクター",
@@ -27,7 +27,7 @@ const genreDefinitions = {
     showAd: true,
     timeLimit: 30,
     showVideo: true,
-    videoUrl: "https://www.youtube.com/embed/3tmd-ClpJxA?autoplay=1"
+    videoUrl: "https://www.youtube.com/embed/1N4x0n2Xl6g?autoplay=1"
   },
   youtube: {
     label: "YouTube",
@@ -35,22 +35,6 @@ const genreDefinitions = {
     showAd: true,
     timeLimit: 30,
     showVideo: true,
-    videoUrl: "https://www.youtube.com/embed/L_jWHffIx5E?autoplay=1"
-  },
-  intermediate_game: {
-    label: "中級ゲーム",
-    key: "game",
-    showAd: true,
-    timeLimit: 20,
-    showVideo: true,
-    videoUrl: "https://www.youtube.com/embed/9bZkp7q19f0?autoplay=1"
-  },
-  advanced_game: {
-    label: "上級ゲーム",
-    key: "game",
-    showAd: true,
-    timeLimit: 15,
-    showVideo: true,
-    videoUrl: "https://www.youtube.com/embed/fJ9rUzIMcZQ?autoplay=1"
+    videoUrl: "https://www.youtube.com/embed/5z5JYwKzYgI?autoplay=1"
   }
 };
