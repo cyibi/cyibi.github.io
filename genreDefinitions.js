750eda57556123c0f9f082bbaa895a140f30ece1
cyibi <cyibila@gmail.com>
2025-08-11 22:52:56 +0900

    Create genreDefinitions.js

---
+++ genreDefinitions.js
@@ -0,0 +1,40 @@
+const genreDefinitions = {
+  game: {
+    label: "ゲーム",
+    key: "game",
+    showAd: false,
+    timeLimit: 0,
+    showVideo: false
+  },
+  anime: {
+    label: "アニメ",
+    key: "anime",
+    showAd: false,
+    timeLimit: 0,
+    showVideo: false
+  },
+  animal: {
+    label: "動物",
+    key: "animal",
+    showAd: true,
+    timeLimit: 30,
+    showVideo: true,
+    videoUrl: "https://www.youtube.com/embed/0Bmhjf0rKe8?autoplay=1"
+  },
+  character: {
+    label: "キャラクター",
+    key: "character",
+    showAd: true,
+    timeLimit: 30,
+    showVideo: true,
+    videoUrl: "https://www.youtube.com/embed/1N4x0n2Xl6g?autoplay=1"
+  },
+  youtube: {
+    label: "YouTube",
+    key: "youtube",
+    showAd: true,
+    timeLimit: 30,
+    showVideo: true,
+    videoUrl: "https://www.youtube.com/embed/5z5JYwKzYgI?autoplay=1"
+  }
+};
